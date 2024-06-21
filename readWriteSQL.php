6ebe9fe04a6252fccd6b30fdc46fbcd2aba068b0
zahernaher <74210661+zahernaher@users.noreply.github.com>
2024-06-21 14:12:00 +0300

    sql user pass

--- readWriteSQL.php
+++ readWriteSQL.php
@@ -4,7 +4,7 @@ try {
     $pdo = new PDO(
         'mysql:host=localhost;dbname=booking',
         'root',
-        '',
+        'root',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     echo "Невозможно установить соединение с базой данных";
